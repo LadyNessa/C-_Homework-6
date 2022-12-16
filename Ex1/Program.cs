0bf0e0c641bc45b471a61178b10785d44bda4b04
Nessa <la-nessa@mail.ru>
2022-12-16 23:26:25 +0300

    Ex1

---
+++ Ex1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Сколько чисел Вы будете вводить?");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int count = 0; //счетчик положительных чисел
+for (int times = 0; times < M; times++)
+{
+    Console.WriteLine("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0) count++;
+}
+
+Console.Write("Количество положительных чисел: ");
+Console.WriteLine(count);
